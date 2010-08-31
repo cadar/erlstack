ce095fc5fa07316b72bc9be06142fee45e4c5ff1
cadar <cadar777>
2010-08-31 18:36:36 +0200

    refactoring and moving words out

--- src/st.erl
+++ src/st.erl
@@ -2,57 +2,61 @@
 -import(lists,[sublist/2,nthtail/2,reverse/1,map/2]).
 -export([run/0,test/0,st/4]).
 % Ws = Words, D = Dictionary, S = Stack, C = Context state interpret/compile => in/comp
+% T = Top of stack, Nextw = Next word in program,
 st([],D,S,C) -> {[],D,S,C};
 
-st(['<<' |Ws], D,          S, in) -> st(reverse(Ws), D, S, in);
-st(['+'  |Ws], D, [Top,S2|S], in) -> st(Ws, D, [Top+S2|S], in);
-st([list |Ws], D,    [Top|S], in) -> st(Ws, D, [sublist(S, Top)|nthtail(Top, S)], in);
-st([drop |Ws], D,      [_|S], in) -> st(Ws, D, S, in);
-st(['.'  |Ws], D,    [Top|S], in) -> io:format("~p", [Top]), st(Ws, D, S, in);
-st(['.s' |Ws], D,          S,  C) -> io:format("s~p~n", [S]), st(Ws, D, S, C);
-st(['.d' |Ws], D,          S,  C) -> io:format("d(~p): ~p~n", [C, D]), st(Ws, D, S, C);
+st(['<<' |Ws], D,        S, in) -> st(reverse(Ws), D, S, in);
+st(['+'  |Ws], D, [T,S2|S], in) -> st(Ws, D, [T+S2|S], in);
+st([list |Ws], D,    [T|S], in) -> st(Ws, D, [sublist(S, T)|nthtail(T, S)], in);
+st([drop |Ws], D,    [_|S], in) -> st(Ws, D, S, in);
+st([mark |Ws], D,        S,  C) -> st(Ws, D, S, C);
+st([l2t  |Ws], D,    [T|S],  C) -> st(Ws, D, [list_to_tuple(T)|S], C);
+st(['#'  | _], D,        S, in) -> st([], D, S, in);
+st(['.'  |Ws], D,    [T|S], in) -> io:format("~p", [T]), st(Ws, D, S, in);
+st(['.s' |Ws], D,        S,  C) -> io:format("s~p ~s~n", [S, C]), print_dic(D), st(Ws, D, S, C);
+st(['.w' |Ws], D,        S,  C) -> io:format("w~p ~s~n", [Ws, C]), st(Ws, D, S, C);
 
-st([create|Ws],                  D, [Top|S], in) -> st(Ws, [{Top,[]}|D],         S, in);
-st([','   |Ws],        [{W,Def}|D], [Top|S], in) -> st(Ws, [{W,Def++[Top]}|D],   S, in);
-st(['\'',   Nextw|Ws],           D,       S, in) -> st(Ws,  D, [Nextw|S], in);
-st(['[\']', Nextw|Ws], [{W,Def}|D],     S, comp) -> st(Ws, [{W,Def++[Nextw]}|D], S, comp);
+%st([create|Ws],                  D, [T|S], in) -> st(Ws,   [{T,[]}|D],         S, in);
+st([create      ,W|Ws],          D,     S, in) -> st(Ws, [{W,[]}|D], S, in);
+st([create_next   |Ws],          D,     S, in) -> {Here,Ws1} = here(Ws,hd(Ws),[]),
+						  st(Ws1, [{Here,[]}|D], S, in);
+st([get_next |Ws],               D,     S, in) -> {Here,Ws1} = here(Ws,hd(Ws),[]),
+						  st(Ws1, D, [Here|S], in);
+st([','      |Ws],     [{W,Def}|D], [T|S], in) -> st(Ws,   [{W,Def++[T]}|D],   S, in);
+st(['\'',   Nextw|Ws],           D,     S, in) -> st(Ws,            D, [Nextw|S], in);
+st(['[\']', Nextw|Ws], [{W,Def}|D],   S, comp) -> st(Ws, [{W,Def++[Nextw]}|D], S, comp);
 
-st(['[[' |Ws], D,          S,  _) -> st(Ws, D, S, in);
-st([']]' |Ws], D,          S,  _) -> st(Ws, D, S, comp);
+st(['.[' |Ws], D, S,  _) -> st(Ws, D, S, in);
+st(['].' |Ws], D, S,  _) -> st(Ws, D, S, comp);
 
-%; = : ; ['] [[ [[ IMM
-% : IMM
-st([':',  W|Ws], D ,S , in)          -> st([create,']]'|Ws], D, [W|S], in);
-st([';'|Ws], D ,S , comp)          -> st(['[['|Ws], D, S, comp);
 st(['IMM'  |Ws], [{W,Def}|D], S, in) -> st(Ws ,[{W,{im,Def}}|D], S, in);
 
 % Compile anonymous function
-st(['{'  |Ws], D, S, in)                -> st([tup,create,']]'|Ws], D, S, in);
-st(['}'  |Ws], [{tup,Comp}|D], S, comp) -> st(['[['|Ws], D, [list_to_tuple(Comp)|S], comp);
-st(['['  |Ws], D, S, _)                 -> st([tmp,create,']]'|Ws], D, S, in);
-st([']'  |Ws], [{tmp,Comp}|D], S, comp) -> st(['[['|Ws], D, [Comp|S], comp);
+st([pop_word|Ws], [{tmp,Comp}|D], S, in) -> st(Ws, D, [Comp|S], in);
 st([apply|Ws], D,[Body|S], in)          -> st(Body++Ws, D, S, in);
 st([call |Ws], D,[Fun,Mod,Args|S], in)  -> Res = apply(Fun, Mod, Args),
                                            st(Ws, D, [Res|S], in);
-%% 1. force execution of word, no compilning into word on dic, 
+%% 1. force execution of word, no compilning into word on dic,
 %%    when done go back to compiling
 %% 2. normal case, compiling to top of dictionary
 st([W|Ws], [{Name,Def}|D], S, comp) ->
     case proplists:lookup(W,D) of
-	{_, {im, Word}} -> 
+	{_, {im, Word}} ->
             st(Word++Ws, [{Name,Def}|D], S, in);
-	_ -> st(Ws, [{Name,Def++[W]}|D], S, comp)
+	_ ->
+	    % io:format("comp-> ~p ~p~n",[Name,W]),
+	    st(Ws, [{Name,Def++[W]}|D], S, comp)
     end;
 
 st([W|Ws], D, S, in) ->
-    case proplists:lookup(W, D) of 
+    case proplists:lookup(W, D) of
 	none -> st(Ws, D, [W|S], in);              % Add to stack
-	{_, {im, Word}} -> 
-            io:format("-> ~p~n",[Word++Ws]),
-            st(Word++Ws, D, S, in); % same as next line
-	{_, Word} -> st(Word++Ws, D, S, in)        % lookup word, add in front of
+	{_, {im, Word}} -> st(Word++Ws, D, S, in); % same as next line
+	{_, Word} ->
+	    % io:format("in-> ~p~n",[Word]),
+	    st(Word++[mark|Ws], D, S, in) % lookup word, add in front of
     end.
-     
+
 run() -> {ok, Binary} = file:read_file("init.st"),
          InitStack = string:tokens(erlang:binary_to_list(Binary), "\n"),
          run(InitStack, [], [], in).
@@ -61,17 +65,26 @@ run([], D, S, C) -> case io:get_line('') of
                         Line -> run([Line], D, S, C)
                     end;
 run([Line|L], D, S, C) ->  Ws = parse(Line),
-                           {_ ,D1 ,S1 ,C1} = st(Ws ,D ,S ,C), 
+                           {_ ,D1 ,S1 ,C1} = st(Ws ,D ,S ,C),
                            run(L, D1 ,S1 ,C1).
 parse(Line) ->
     NoLineBreak = re:replace(Line, "\n", "", [{return,list}]),
     Toks        = string:tokens(NoLineBreak, " "),
     AtomToks    = map(fun(X) -> list_to_atom(X) end, Toks),
-    Ws          = map(fun(X) -> try list_to_integer(atom_to_list(X)) 
+    Ws          = map(fun(X) -> try list_to_integer(atom_to_list(X))
                                 catch _:_ -> X end end, AtomToks),
     Ws.
 
 test() -> 0.
 
+print_dic(D) -> map(fun(X) -> io:format("    ~p~n",[X]) end, D).
 
-d(S) -> io:format("~s~n",[S]).
+here([],First,Acc) -> {First,tl(reverse(Acc))};
+here([mark,W|Ws],_,Acc) -> {W,Acc++Ws};
+here([W|Ws],PassOn,Acc) -> here(Ws,PassOn,[W|Acc]).
+
+% 99%
+%create_next ;; ]. ['] .[ .[ IMM
+%create_next :: ]. create_next ['] ]. ;;
+%:: hej 1 ;;
+
